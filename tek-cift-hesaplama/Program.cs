445e12f4d1233bcd4c1262e503617ed70dd05162
Serhatkoluk <154443083+Serhatkoluk@users.noreply.github.com>
2024-01-14 21:28:49 +0300

    tek cift hesaplaması

---
+++ tek-cift-hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tek_cift_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int a, b = 2, mod;
+                Console.WriteLine("Bir sayı giriniz");
+                a = Convert.ToInt32(Console.ReadLine());
+                mod = (a % b);
+                if (mod == 0)
+                    Console.WriteLine("Girilen sayi çift sayıdır.");
+                else
+                    Console.WriteLine("Girilen sayi tek sayidir.");
+                Console.ReadKey();
+
+            }
+    }
+}
